tmfink-juniper/xinetd

commit 56520dcc88c35113877c8f4f52bd98ad95b44653

Remove most of the always true configure checks and convert the required ones to pc/regular checks

src/confparse.c
@@ -13,18 +13,9 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-
-#ifdef HAVE_RPC_RPC_H
 #include <rpc/rpc.h>
-#endif
-
-#ifdef HAVE_RPC_RPCENT_H
 #include <rpc/rpcent.h>
-#endif
-
-#ifdef HAVE_NETDB_H
 #include <netdb.h>
-#endif
 
 #include "str.h"
 #include "sio.h"
@@ -632,7 +623,6 @@ static status_e check_entry( struct service_config *scp,
           */
          continue;
       }
-#if defined(HAVE_RPC_RPCENT_H) || defined(HAVE_NETDB_H)
       if ( SC_IS_RPC( scp ) && SC_IS_RPC ( tmp_scp ) )
       {
          const struct rpc_data *rdp1 = SC_RPCDATA( scp ) ;
@@ -643,7 +633,6 @@ static status_e check_entry( struct service_config *scp,
              rdp1->rd_max_version < rdp2->rd_min_version )
           continue;
       }
-#endif
       if (diff) 
          msg( LOG_ERR, func, 
          "service: %s id: %s is unique but its identical to "
@@ -746,7 +735,6 @@ static status_e check_entry( struct service_config *scp,
    }
 
 /* #ifndef NO_RPC */
-#if defined(HAVE_RPC_RPCENT_H) || defined(HAVE_NETDB_H)
    if ( SC_IS_RPC( scp ) && !SC_IS_UNLISTED( scp ) )
    {
       struct rpcent *rep = (struct rpcent *)getrpcbyname( SC_NAME(scp) ) ;
@@ -759,7 +747,6 @@ static status_e check_entry( struct service_config *scp,
       SC_RPCDATA( scp )->rd_program_number = rep->r_number ;
    }
    else
-#endif   /* ! NO_RPC */
    {
        if ( !SC_IS_UNLISTED( scp ) ) 
        { 

Makefile.am
@@ -70,17 +70,9 @@ libmisc_la_SOURCES = \
 	src/misc/misc.c \
 	src/misc/misc.h
 libportable_la_SOURCES = \
-	src/portable/compat.h \
 	src/portable/cvt.c \
-	src/portable/difftime.c \
-	src/portable/fake-getaddrinfo.c \
-	src/portable/fake-getaddrinfo.h \
-	src/portable/fake-getnameinfo.c \
-	src/portable/inet_aton.c \
-	src/portable/inet_ntop.c \
 	src/portable/libportable.h \
-	src/portable/strcasecmp.c \
-	src/portable/strerror.c
+	src/portable/strcasecmp.c
 libpset_la_SOURCES = \
 	src/pset/ops.c \
 	src/pset/pset.c \
@@ -122,7 +114,8 @@ sbin_PROGRAMS = xinetd
 xinetd_CFLAGS = \
 	$(HOWL_CFLAGS) \
 	$(HARDEN_CFLAGS) \
-	$(SELINUX_CFLAGS)
+	$(SELINUX_CFLAGS) \
+	$(TIRPC_CFLAGS)
 xinetd_SOURCES = \
 	src/access.c \
 	src/access.h \
@@ -217,7 +210,8 @@ xinetd_LDADD = \
 	libxlog.la \
 	$(HOWL_LIBS) \
 	$(WRAP_LIBS) \
-	$(SELINUX_LIBS)
+	$(SELINUX_LIBS) \
+	$(TIRPC_LIBS)
 
 distclean-local:
 	rm -rf *.cache *~

configure.ac
@@ -30,56 +30,27 @@ AC_PROG_LN_S
 AC_PROG_MAKE_SET
 AC_PROG_LIBTOOL
 PKG_PROG_PKG_CONFIG([0.20])
+AC_CHECK_LIB([m], [log10], [
+	LIBM_LIBS="-lm"
+], [
+	AC_MSG_ERROR([Unable to find working libm.so])
+])
+AC_SUBST([LIBM_LIBS])
+AC_CHECK_LIB([crypt], [main], [
+	CRYPTO_LIBS="-lcrypto"
+	AC_DEFINE([HAVE_LIBCRYPT], [1], [Have libcrypt.so present])
+], [
+	AC_MSG_ERROR([Unable to find working libcrypto.so])
+])
+AC_SUBST([CRYPTO_LIBS])
+PKG_CHECK_MODULES([TIRPC],[libtirpc], [],[AC_MSG_ERROR([Missing libtirpc libraries])])
 
 # ======================================
 # Check for various headers and settings
 # ======================================
-AC_CHECK_FUNCS(getnameinfo inet_ntop)
-AC_CHECK_FUNCS(difftime, ,
-   AC_CHECK_LIB(m, difftime))
-AC_CHECK_FUNCS(fcvt, ,
-   AC_CHECK_LIB(m, fcvt))
-AC_CHECK_FUNCS(ecvt, ,
-   AC_CHECK_LIB(m, ecvt))
-AC_CHECK_FUNCS(gcvt, ,
-   AC_CHECK_LIB(m, gcvt))
-AC_CHECK_FUNC(strerror, [AC_DEFINE(HAVE_STRERROR, 1, "")])
-AC_CHECK_FUNC(strcasecmp, [AC_DEFINE(HAVE_STRCASECMP, 1, "")])
-AC_CHECK_FUNC(socket, ,
-   AC_CHECK_LIB(socket, socket, ,
-      AC_CHECK_LIB(nsl, socket)))
-AC_CHECK_FUNC(inet_aton, ,
-	AC_CHECK_LIB(nsl, inet_aton, ,
-		AC_CHECK_LIB(socket, inet_aton, ,
-			AC_CHECK_LIB(resolv, inet_aton, ,[AC_DEFINE(NO_INET_ATON, 1, "")]))))
-AC_CHECK_FUNC(setenv,[AC_DEFINE(HAVE_SETENV, 1, "")])
-AC_CHECK_FUNC(strsignal, [AC_DEFINE(HAVE_STRSIGNAL, 1, "")])
-AC_CHECK_LIB(c, sys_siglist, [AC_DEFINE(HAVE_SYS_SIGLIST, 1, "")])
-AC_CHECK_FUNC(gai_strerror,[AC_DEFINE(HAVE_GAI_STRERROR, 1, "")])
-AC_CHECK_FUNC(freeaddrinfo,[AC_DEFINE(HAVE_FREEADDRINFO, 1, "")])
-AC_CHECK_FUNC(getaddrinfo,[AC_DEFINE(HAVE_GETADDRINFO, 1, "")])
-AC_CHECK_HEADERS(sys/types.h sys/termios.h termios.h sys/ioctl.h sys/select.h rpc/rpc.h rpc/rpcent.h sys/file.h ftw.h machine/reg.h netdb.h)
-AC_CHECK_HEADER(sys/resource.h, [AC_DEFINE(HAVE_SYS_RESOURCE_H, 1, "")])
-AC_CHECK_HEADER(arpa/inet.h, [AC_DEFINE(HAVE_ARPA_INET_H, 1, "")])
-AC_CHECK_HEADER(grp.h, [AC_DEFINE(HAVE_GRP_H, 1, "")])
-AC_CHECK_HEADER(rpc/pmap_clnt.h, [AC_DEFINE(HAVE_RPC_PMAP_CLNT_H, 1, "")])
-AC_CHECK_HEADER(sys/socket.h, [AC_DEFINE(HAVE_SYS_SOCKET_H, 1, "")])
-AC_CHECK_HEADER(sys/signal.h, [AC_DEFINE(HAVE_SYS_SIGNAL_H, 1, "")])
-AC_CHECK_HEADER(crypt.h, [AC_DEFINE(HAVE_CRYPT_H, 1, "")])
-AC_CHECK_HEADER(stdint.h, [AC_DEFINE(HAVE_STDINT_H, 1, "")])
-AC_CHECK_HEADER(stdbool.h, [AC_DEFINE(HAVE_STDBOOL_H, 1, "")])
+AC_FUNC_MMAP
 AC_CHECK_HEADER(sys/filio.h, [AC_DEFINE(HAVE_SYS_FILIO_H, 1, "")])
 AC_CHECK_HEADER(DNSServiceDiscovery/DNSServiceDiscovery.h, [AC_DEFINE(HAVE_DNSREGISTRATION, 1, "") AC_DEFINE(HAVE_MDNS, 1, "")])
-AC_FUNC_MMAP
-AC_CHECK_FUNCS(isatty)
-AC_CHECK_FUNCS(memcpy)
-AC_CHECK_FUNCS(waitpid)
-AC_CHECK_FUNCS(sigvec)
-AC_CHECK_FUNCS(setsid)
-AC_CHECK_FUNCS(strftime)
-AC_CHECK_LIB(c, crypt, [:], [AC_CHECK_LIB(crypt, crypt, [LIBS="-lcrypt $LIBS" AC_DEFINE(HAVE_LIBCRYPT, 1, "") ], []) ])
-AC_CHECK_LIB(m, log10, [ LIBS="-lm $LIBS" ], [])
-AC_CHECK_LIB(crypt, main)
 
 # ======
 # Switch

src/addr.c
@@ -19,13 +19,9 @@
 #include <unistd.h>
 #include <sys/param.h>
 #include <string.h>
-#ifdef HAVE_NETDB_H
 #include <netdb.h>
-#endif
 
-#if defined(HAVE_ARPA_INET_H)
 #include <arpa/inet.h>
-#endif
 #include <netinet/in.h>
 
 #include "sio.h"

src/builtins.c
@@ -16,9 +16,7 @@
 #include <syslog.h>
 #include <stdlib.h>
 #include <unistd.h>
-#ifdef HAVE_NETDB_H
 #include <netdb.h>
-#endif
 
 #include "str.h"
 #include "pset.h"
@@ -237,18 +235,10 @@ static void daytime_protocol( char *buf, unsigned int *buflen )
    time_t      now ;
    struct tm   *tmp ;
    int         size = *buflen ;
-#ifdef HAVE_STRFTIME
    int      cc ;
-#endif
 
    (void) time( &now ) ;
    tmp = localtime( &now ) ;
-#ifndef HAVE_STRFTIME
-   strx_print( buflen, buf, size,
-      "%02d %s %d %02d:%02d:%02d %s\r\n",
-         tmp->tm_mday, month_name[ tmp->tm_mon ], 1900 + tmp->tm_year,
-            tmp->tm_hour, tmp->tm_min, tmp->tm_sec, tmp->tm_zone ) ;
-#else
    cc = strx_nprint( buf, size,
       "%02d %s %d %02d:%02d:%02d",
          tmp->tm_mday, month_name[ tmp->tm_mon ], 1900 + tmp->tm_year,
@@ -259,7 +249,6 @@ static void daytime_protocol( char *buf, unsigned int *buflen )
       cc = strftime( &buf[ *buflen ], (size_t)size, " %Z\r\n", tmp ) ;
       *buflen += cc ;
    }
-#endif
 }
 
 

src/child.c
@@ -10,9 +10,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/time.h>
-#ifdef HAVE_SYS_RESOURCE_H
 #include <sys/resource.h>
-#endif
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <netinet/in.h>
@@ -25,12 +23,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-#if defined (HAVE_GRP_H)
 #include <grp.h>
-#endif
-#ifdef HAVE_NETDB_H
 #include <netdb.h>
-#endif
 #ifdef LABELED_NET
 #include <selinux/selinux.h>
 #include <selinux/flask.h>
@@ -175,13 +169,7 @@ void exec_server( const struct server *serp )
    (void) Sclose( descriptor ) ;
 
 #ifndef solaris
-#if !defined(HAVE_SETSID)
-   msg_resume();
-#endif
    no_control_tty() ;
-#if !defined(HAVE_SETSID)
-   msg_suspend();
-#endif
 #endif
 
    (void) execve( server, SC_SERVER_ARGV( scp ),
@@ -397,7 +385,6 @@ void child_process( struct server *serp )
       }
       else
       {
-#if defined(HAVE_SETENV)
          char buff[1024];
 
          strx_sprint(buff, sizeof(buff)-1, "REMOTE_HOST=%s", conn_addrstr(cp));
@@ -405,7 +392,6 @@ void child_process( struct server *serp )
             msg( LOG_ERR, func, "Error adding REMOTE_HOST variable for %s: %m", SC_NAME(scp) );
             _exit( 1 ) ;
          }
-#endif
          exec_server( serp ) ;
       }
    }
@@ -470,22 +456,10 @@ void child_exit(void)
       pid_t pid ;
       struct server *serp ;
       
-#ifdef HAVE_WAITPID
       pid = waitpid( -1, &status, WNOHANG ) ;
-#else
-#if defined( sun ) && defined( lint )
-      pid = wait3( (union wait *)&status, WNOHANG, RUSAGE_NULL ) ;
-#else
-      pid = wait3( &status, WNOHANG, RUSAGE_NULL ) ;
-#endif
-#endif
 
       if ( debug.on )
-#ifdef HAVE_WAITPID
          msg( LOG_DEBUG, func, "waitpid returned = %d", pid ) ;
-#else
-         msg( LOG_DEBUG, func, "wait3 returned = %d", pid ) ;
-#endif
       
       if ( pid == -1 ) {
          if ( errno == EINTR )

src/connection.h
@@ -15,9 +15,7 @@
 #include "config.h"
 #include <sys/types.h>
 #include <netinet/in.h>
-#if defined( HAVE_ARPA_INET_H )
 #include <arpa/inet.h>
-#endif
 #include <string.h>
 
 #include "mask.h"

src/init.c
@@ -9,9 +9,7 @@
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/stat.h>
-#ifdef HAVE_SYS_RESOURCE_H
 #include <sys/resource.h>
-#endif
 #include <syslog.h>
 #include <fcntl.h>
 #include <unistd.h>

commit 4278495b1858b5b1ce94d72bd619eb79aab8ca04

fix no-rpc builds
The code base already has support for -DNO_RPC, so fix the breakage
introduced by commit 56520dcc88c35113877c8f4f52bd98ad95b44653.

src/confparse.c
@@ -13,9 +13,11 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#ifndef NO_RPC
 #include <rpc/rpc.h>
 #ifdef HAVE_RPCENT_H
 #include <rpc/rpcent.h>
+#endif
 #endif
 #include <netdb.h>
 
@@ -730,7 +732,7 @@ static status_e check_entry( struct service_config *scp,
 	   }
    }
 
-/* #ifndef NO_RPC */
+#ifndef NO_RPC
    if ( SC_IS_RPC( scp ) && !SC_IS_UNLISTED( scp ) )
    {
       struct rpcent *rep = (struct rpcent *)getrpcbyname( SC_NAME(scp) ) ;
@@ -743,6 +745,7 @@ static status_e check_entry( struct service_config *scp,
       SC_RPCDATA( scp )->rd_program_number = rep->r_number ;
    }
    else
+#endif
    {
        if ( !SC_IS_UNLISTED( scp ) ) 
        { 